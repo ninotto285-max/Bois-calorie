a6d7440e389a0511eb94e13254ce1090c6b352f7
ninotto285-max <ninotto285@gmail.com>
2026-05-24 22:14:28 +0200

    Update Clienti.js

--- api/Clienti.js
+++ api/Clienti.js
@@ -17,7 +17,7 @@ async function supabaseFetch(path, options = {}) {
   return text ? JSON.parse(text) : null;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
