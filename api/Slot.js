a43d49a352a52b39da0e8646e33c0d82f610f843
ninotto285-max <ninotto285@gmail.com>
2026-05-24 22:13:46 +0200

    Update Slot.js

--- api/Slot.js
+++ api/Slot.js
@@ -56,7 +56,7 @@ async function getSlotInfo(data, orario) {
   return { pizze_count: 0, slot_esclusivo: false };
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
